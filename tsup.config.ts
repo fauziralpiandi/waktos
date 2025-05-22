196fd1a43145d1dfaa3a02ec4c5aa9209caa7a9e
Fauzira Alpiandi <fwzyrln@gmail.com>
2025-05-22 09:52:13 +0000

    build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   format: ['esm'],
   entry: ['src/index.ts'],
-  target: 'es2017',
+  target: 'ES2020',
   outDir: 'dist',
   dts: true,
   clean: true,
   minify: true,
   splitting: true,
   treeshake: true,
+  esbuildOptions(options) {
+    options.charset = 'utf8';
+  },
 });
